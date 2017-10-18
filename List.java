e93ceb0b50a131799cab2f8a6f7fdd143f5a4826
OmegaHelix <dakotaf@sasktel.net>
2017-10-17 18:30:56 -0600

    Created List Class
    Created List class with constructor

---
+++ List.java
@@ -0,0 +1,19 @@
+import ListElement.java;
+
+public class List
+{
+    private ListElement head;
+    private ListElement tail;
+
+
+    public List()
+    {
+        this.head = null;
+        this.tail = null;
+    
+    }
+
+    
+
+
+}
